3ca5780b3d7c60fe143f86e6b7aca91da65561de
madhavsameer <madhavsameer97@gmail.com>
2024-02-23 19:56:41 +0530

    added responsiveness and footer

--- src/StudentDetails.js
+++ src/StudentDetails.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './StudentDetails.css'; // Import CSS file for styling
 
+import { faInstagram, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
 const StudentDetails = () => {
     const [regNo, setRegNo] = useState('');
     const [studentData, setStudentData] = useState([]);
@@ -23,7 +28,7 @@ const StudentDetails = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.get(`https://cutm-result-backend.onrender.com/${regNo}`);
+            const response = await axios.get(`https://cutm-result-backend.onrender.com/api/students/${regNo}`);
             setStudentData(response.data); // Update studentData state with response data
             setStudentName(response.data[0]?.Name || '');
             setRegistrationNumber(response.data[0]?.Reg_No || '');
@@ -98,8 +103,8 @@ const StudentDetails = () => {
             <h1 className="title">Student Report Card</h1>
             <form onSubmit={handleSubmit} className="search-form">
                 <label>
-                    Enter Registration Number:
-                    <input type="text" value={regNo} onChange={handleChange} className="input-field" />
+                    
+                    <input type="text" value={regNo} onChange={handleChange} className="input-field" placeholder='Enter Registration Number:' />
                 </label>
                 <button type="submit" className="submit-button">Search</button>
             </form>
@@ -125,10 +130,11 @@ const StudentDetails = () => {
                         <thead>
                             <tr>
                                 <th>Sl No</th>
-                                <th>Reg No</th>
-                                <th>Name</th>
-                                <th>Subject Code</th>
+                                {/* <th>Reg No</th>
+                                <th>Name</th> */}
                                 <th>Subject Name</th>
+                                <th>Subject Code</th>
+                                
                                 <th>Type</th>
                                 <th>Credits</th>
                                 <th>Grade</th>
@@ -138,10 +144,11 @@ const StudentDetails = () => {
                             {studentData.map((student, index) => (
                                 <tr key={index}>
                                     <td>{student["Sl No"]}</td>
-                                    <td>{student.Reg_No}</td>
-                                    <td>{student.Name}</td>
-                                    <td>{student.Subject_Code}</td>
+                                    {/* <td>{student.Reg_No}</td>
+                                    <td>{student.Name}</td> */}
                                     <td>{student.Subject_Name}</td>
+                                    <td>{student.Subject_Code}</td>
+                                    
                                     <td>{student.Type}</td>
                                     <td>{student.Credits}</td>
                                     <td>{student.Grade}</td>
@@ -151,10 +158,36 @@ const StudentDetails = () => {
                     </table>
                     <p className="cgpa">CGPA: {cgpa.toFixed(2)}</p>
                     
+                    
                 </div>
             )}
             
+            
         </div>
+        <div id='footer'>
+               <p> Designed & Developed by Madhav Sameer</p>
+               <p> Copyright @2024 | All Rights Reserved</p>
+               <ul className="footer__links">
+          <li className="footer__link-item">
+            <a href="https://www.instagram.com/aditya.exeeeee" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faInstagram} className="footer__icon" />
+            </a>
+          </li>
+          <li className="footer__link-item">
+            <a href="https://www.linkedin.com/in/madhavsameer" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faLinkedin} className="footer__icon" />
+            </a>
+          </li>
+          <li className="footer__link-item">
+            <a href="https://github.com/madhavsameer" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faGithub} className="footer__icon" />
+            </a>
+          </li>
+          {/* Add more social media links as needed */}
+        </ul>
+        
+               
+            </div>
         
         </div>
     );
